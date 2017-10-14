4a2c8043d75d723aa76e9b29cf46a798a11e2a09
warlyware <danward@gmail.com>
2017-10-14 14:52:39 -0400

    Add basic search bar component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { SearchBar } from './components/search-bar';
+
 import { ApiKeys } from '../api-keys.constant.js';
 
 const API_KEY = ApiKeys.youtube;
 
 const App = () => {
-  return <div>hello world</div>
+  return(
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
