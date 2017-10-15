98d107a17b36aa8ea278ca5d8165e3a6ea2f93b4
warlyware <danward@gmail.com>
2017-10-15 18:00:40 -0400

    Add super call, state

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 
 export class SearchBar extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      term: ''
+    };
+  }
+
   render() {
     return <input onChange={this.onInputChange} />;
   }
