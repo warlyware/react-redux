3e8f63a29e4a83d625fac2d4dec9d125f2424fd8
warlyware <danward@gmail.com>
2017-10-16 13:44:15 -0400

    Build out video detail markup

---
+++ src/components/video-detail.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export const VideoDetail = ({ video }) => {
+  const videoId = video.id.videoId;
+  const url = `//www.youtube.com/embed/${videoId}`;
+
+  return(
+    <div className="video-detail col-md-8">
+      <div className="embed-responsive embed-responsive-16by9">
+        <iframe src={url} className="embed-responsive-item"></iframe>
+      </div>
+      <div className="details">
+        <div>{video.snippet.title}</div>
+        <div>{video.snippet.description}</div>
+      </div>
+    </div>
+  );
+}
