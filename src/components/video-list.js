332585abffbbec7c14f1961a3cd1a97ff334899c
warlyware <danward@gmail.com>
2017-10-16 13:01:40 -0400

    Add basic vido list component

---
+++ src/components/video-list.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const VideoList = (props) => {
+  return (
+    <ul className="col-md-4">
+      <li>{propse.videos.length}</li>
+    </ul>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import { SearchBar } from './components/search-bar';
+import { VideoList } from './components/video-list'
 
 import { ApiKeys } from '../api-keys.constant.js';
 
 const API_KEY = ApiKeys.youtube;
 
+YTSearch({
+  key: API_KEY,
+  term: 'bacon',
+}, (data) => {
+  console.log(data);
+});
+
 const App = () => {
   return(
     <div>
       <SearchBar />
+      <VideoList videos={this.state.videos} />
     </div>
   );
 }
