10cbfad518cf17d35607d4d3caeaa2a55c8e96e1
warlyware <danward@gmail.com>
2017-10-16 13:27:59 -0400

    Map over videos, render VideoListItem for each

--- src/components/video-list.js
+++ src/components/video-list.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import VideoListItem from './video-list-item';
+import { VideoListItem } from './video-list-item';
 
 export const VideoList = (props) => {
-  debugger;
+  const videoItems = props.videos.map((video) => {
+    return <VideoListItem video={video} />
+  });
+
   return (
     <ul className="col-md-4">
-
+      {videoItems}
     </ul>
   );
 }
